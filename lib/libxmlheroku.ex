f4c192096033aea9f307e611151911bee0da283c
Thomas <thomas@icdesign.com.au>
2018-04-06 15:08:24 +1000

    Initial Elixir app

---
+++ lib/libxmlheroku.ex
@@ -0,0 +1,18 @@
+defmodule Libxmlheroku do
+  @moduledoc """
+  Documentation for Libxmlheroku.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Libxmlheroku.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/libxmlheroku_test.exs
@@ -0,0 +1,8 @@
+defmodule LibxmlherokuTest do
+  use ExUnit.Case
+  doctest Libxmlheroku
+
+  test "greets the world" do
+    assert Libxmlheroku.hello() == :world
+  end
+end
